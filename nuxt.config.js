651c921f961d127e7f772fe9de60636d3b048551
Vino Samari <iamvinomusic@gmail.com>
2023-02-28 14:50:48 +0000

    Updated nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,7 @@ export default {
       { name: "format-detection", content: "telephone=no" },
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: "stylesheet", href: "/assets/css/global.css" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
